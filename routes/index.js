e1b5f4081b7eee14f66abb0736f779cd7ee46820
tyckobra <tycko.bergman@elev.ga.ntig.se>
2023-01-20 11:10:29 +0100

    23-01-20

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+const mysql = require('mysql2');
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+});
+const promisePool = pool.promise();
+
 router.get('/', async function (req, res) {
     res.send('Hello you!')
+});
+
+router.get('/', async function (req, res, next) {
+    const [rows] = await promisePool.query("SELECT * FROM tb02forum");
+    res.json({ rows });
+});
+
+res.render('index.njk', {
+    rows: rows,
+    title: 'Forum',
 });
 
 module.exports = router;
